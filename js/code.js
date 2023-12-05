523fecb59bfe4155352b31cc71a1a0686aa34abd
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-12-05 16:16:37 +0200

    Create clearInput function

--- js/code.js
+++ js/code.js
@@ -1,14 +1,20 @@
 const myLibrary = [];
-
+let count  = 1;
 // select all text inputs
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
+//select book-list div
+const bookList = document.querySelector(".book-list");
 // select add button
 const addButton = document.querySelector(".add");
+//create book-card  div
+let bookCard  = document.createElement("div");
 //add an event listener to addButton
+
 addButton.addEventListener("click",getInputs);
+
 
 //function to get input values:
 function getInputs(event){
@@ -18,8 +24,59 @@ function getInputs(event){
     let book = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
     //pass object to addBookToLibrary function
     addBookToLibrary(book);
-
+    generateBookCards();
+    clearInputs();
+    
+    
+    
 }
+
+function generateBookCards(){
+    bookCard  = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookList.appendChild(bookCard);
+    let coverArt = document.createElement("div");
+    bookCard.appendChild(coverArt);    
+    coverArt.classList.add("cover-art");
+
+    let title = document.createElement("div");
+    let author = document.createElement("div");
+    let pages = document.createElement("div");
+    let read = document.createElement("div");
+
+
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+
+    let firstLetterOfTitle = myLibrary[count-1].title[0].toUpperCase();
+    let titleVal = myLibrary[count-1].title;
+    let authorVal = myLibrary[count-1].author;
+    let pagesVal = myLibrary[count-1].pages;
+    let readVal = myLibrary[count-1].read
+
+    coverArt.innerHTML = firstLetterOfTitle;
+    title.innerHTML = `Title: ${titleVal}`;
+    author.innerHTML = `Author: ${authorVal}`;
+    pages.innerHTML = `Pages: ${pagesVal}`;
+    read.innerHTML = `Read: ${readVal}`;
+
+
+    
+    count++;
+    
+    
+ 
+}
+
+
+function clearInputs(){
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach(singleInput =>singleInput.value = "");
+    
+}         
     
 
 function Book(title,author,pages,read){
@@ -32,4 +89,3 @@ function Book(title,author,pages,read){
 function addBookToLibrary(object){
     myLibrary.push(object);
 }
-
