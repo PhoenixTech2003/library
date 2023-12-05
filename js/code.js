71c923a1fcfeb44e071ed0bb0402779290c24e58
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-12-05 09:42:23 +0200

    Create getInputs function

--- js/code.js
+++ js/code.js
@@ -1,5 +1,27 @@
 const myLibrary = [];
 
+// select all text inputs
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+// select add button
+const addButton = document.querySelector(".add");
+//add an event listener to addButton
+addButton.addEventListener("click",getInputs);
+
+//function to get input values:
+function getInputs(event){
+    //prevent default action of add  button
+    event.preventDefault();
+    // create an object variable and pass input values to object
+    let book = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+    //pass object to addBookToLibrary function
+    addBookToLibrary(book);
+
+}
+    
+
 function Book(title,author,pages,read){
     this.title = title;
     this.author = author;
@@ -7,6 +29,7 @@ function Book(title,author,pages,read){
     this.read = read;
 }
 
-function addBookToLibrary(){
-
+function addBookToLibrary(object){
+    myLibrary.push(object);
 }
+
