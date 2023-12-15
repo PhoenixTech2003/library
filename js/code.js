f2cd104bc6601e68ad396fa275363c11f3da521b
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-12-15 05:12:38 +0200

    Add removeBook function

--- js/code.js
+++ js/code.js
@@ -1,133 +1,166 @@
-const myLibrary = [];
-let count  = 1;
-// select all text inputs
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const pagesInput = document.querySelector("#pages");
-const readInput = document.querySelector("#read-status");
-//select book-list div
-const bookList = document.querySelector(".book-list");
-// select add button
-const addButton = document.querySelector(".add");
-//create book-card  div
-let bookCard  = document.createElement("div");
-const mainContainer = document.querySelector(".main-container");
-const sideBar = document.querySelector(".side-bar");
+const myLibrary = []; //array to store book objects
 
-mainContainer.removeChild(sideBar);
-const newBookButton = document.querySelector(".new-book");
-
-
-//add an event listener to addButton
-
-
-newBookButton.addEventListener("click",displaySideBar);
-addButton.addEventListener("click",getInputs);
-
-function displaySideBar(event){
+const addBookBtn =document.querySelector(".add");//select button to add book to library
+//create an event listener for the addBookBtn to add to library
+addBookBtn.addEventListener("click",function(event){
     event.preventDefault();
-    mainContainer.insertBefore(sideBar,bookList);
-}
+    addBookToLibrary();
+    clearInputFields();
+    generateBookCard();
+});
 
-//function to get input values:
-function getInputs(event){
-    //prevent default action of add  button
-    event.preventDefault();
-    // create an object variable and pass input values to object
+document.addEventListener("click",function(event){
+    //create variable for selected elements
+    let element = event.target;
+    //if element === remove button
+    if(element.className === "remove-book"){
+        //create variable to store remove book data attribute
+        let bookNumber = element.dataset.removeBtnNumber;
+        //call remove book function and pass data attribute
+        removeBook(bookNumber);
+        //call generate book card function
+        generateBookCard();
+    }
     
-        let book = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.options[readInput.selectedIndex].value
-            );
-        //pass object to addBookToLibrary function
-        addBookToLibrary(book);
-    generateBookCards();
-    clearInputs();
-    
-    
-    
-}
-
-function generateBookCards(){
-    bookCard  = document.createElement("div");
-    bookCard.classList.add("book-card");
-    bookCard.dataset.number = count-1;
-    bookList.appendChild(bookCard);
-
-    let coverArt = document.createElement("div");
-    bookCard.appendChild(coverArt);    
-    coverArt.classList.add("cover-art");
-
-    let title = document.createElement("div");
-    let author = document.createElement("div");
-    let pages = document.createElement("div");
-    let read = document.createElement("div");
-    let removeBookButton = document.createElement("button");
-    removeBookButton.dataset.number = count-1;
+});
 
 
 
-
-
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(read);
-    bookCard.appendChild(removeBookButton);
-    removeBookButton.innerHTML = "Remove Book";
-    removeBookButton.classList.add("remove-book");
-
-let removeBook = document.querySelectorAll(".remove-book"); //select all remove buttons
-removeBook.forEach(btn => {
-    btn.addEventListener("click",(event)=>{
-        let bookCardNumber = event.target.dataset.number;
-        bookCard = document.querySelector(`[data-number = '${bookCardNumber}']`);
-        bookList.removeChild(bookCard);
-        myLibrary.splice(bookCardNumber,1);
-        count --;
-    })
-})
-
-
-    let firstLetterOfTitle = myLibrary[count-1].title[0].toUpperCase();
-    let titleVal = myLibrary[count-1].title;
-    let authorVal = myLibrary[count-1].author;
-    let pagesVal = myLibrary[count-1].pages;
-    let readVal = myLibrary[count-1].read
-
-    coverArt.innerHTML = firstLetterOfTitle;
-    title.innerHTML = `Title: ${titleVal}`;
-    author.innerHTML = `Author: ${authorVal}`;
-    pages.innerHTML = `Pages: ${pagesVal}`;
-    read.innerHTML = `Read: ${readVal}`;
-
-
-    
-    count++;
-    
-    
- 
-}
-
-
-function clearInputs(){
-    const inputs = document.querySelectorAll("form > input");
-    inputs.forEach(singleInput =>singleInput.value = "");
-    
-}         
-    
-
-function Book(title,author,pages,read){
+//constructor for creating book objects
+function Book(title, author, pages, readStatus){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.readStatus = readStatus;
+
+
 }
 
-function addBookToLibrary(object){
-    myLibrary.push(object);
+//function that adds a book to myLibrary array
+function addBookToLibrary(){
+
+    let bookDetails = getBookDetails();//variable to store return value of getBookDetails
+
+    let book = new Book(bookDetails.title,bookDetails.author,bookDetails.pages,bookDetails.readStatus);//create object that receives properties of a book
+
+    //push to myLibrary array
+    myLibrary.push(book);
+    
+
+
 }
 
-//add an event listener to all remove buttons of click
+//function to get details from the DOM
+function getBookDetails(){    
+    //select all side-bar input elements text
+    const titleInput = document.querySelector("#title").value;
+    const authorInput = document.querySelector("#author").value;
+    const pagesInput = document.querySelector("#pages").value;
+    const readStatusInput = document.querySelector("#read-status").value;
+
+     //create object to store book details
+    let bookDetails = {
+        title: titleInput,
+        author: authorInput,
+        pages: pagesInput,
+        readStatus:readStatusInput
+    };
+    
+    
+    //return  the book details object
+    return bookDetails;
+}
+
+//function to clear input fields
+function clearInputFields(){
+
+    //select all input fields apart from the option box
+    const inputFields = document.querySelectorAll("form > input");
+    //set value = "" for each input field
+    inputFields.forEach(inputField => inputField.value = "");
+    
+}
+
+//function to create book cards
+function generateBookCard(){
+    const bookListContainer = document.querySelector(".book-list"); //select the book list div to append and remove book cards
+    
+    let lengthOfArray = myLibrary.length; //get length of myLibrary array
+    //remove existing book cards to regenerate after adding new book card
+    const bookCardContainer = document.querySelectorAll(".book-card");
+    bookCardContainer.forEach(bookCard => bookListContainer.removeChild(bookCard));
+    //use for loop to generate book cards
+    for(let index = 0; index < lengthOfArray; index++){
+        
+        let bookCard = document.createElement("div"); //create blank div element for the book cards
+        //append book card to book list div
+        bookListContainer.append(bookCard);
+        //add book card class to book card div
+        bookCard.className = "book-card";
+        bookCard.dataset.cardNumber = index;//add a data atribute to each book card for identification
+        //select the book card using the data atribute
+        bookCard = document.querySelector(`.book-card[data-card-number = "${index}" ]`);
+        const coverArtContainer = document.createElement("div");//create a div for the cover art
+        let coverArtLetter = myLibrary[index].title.charAt(0);//select the first letter of the book title to be used for the cover art
+        //append cover art div to selected book card
+        bookCard.append(coverArtContainer);
+        //add the cover art class to the coveart art div
+        coverArtContainer.className = "cover-art";
+        coverArtContainer.innerHTML = coverArtLetter;//insert the first letter in the inner html of the cover art div
+        //create variable to store title of book
+        let bookTitle = myLibrary[index].title;
+        //create variable to store author of book
+        let bookAuthor = myLibrary[index].author;
+        //create variable to store pages of book
+        let bookPages = myLibrary[index].pages;
+        //create variable to store read status of book
+        let bookReadStatus = myLibrary[index].readStatus;
+
+        //create div to store the book title
+        let bookTitleContainer = document.createElement("div");
+        //append book title div to book card
+        bookCard.append(bookTitleContainer);
+        bookTitleContainer.innerHTML =`Title: ${bookTitle}` ;//set inner html = bookTile
+        
+
+        //create div to store the book author
+        let bookAuthorContainer = document.createElement("div");
+        //append book title div to book card
+        bookCard.append(bookAuthorContainer);
+        bookAuthorContainer.innerHTML =`Author: ${bookAuthor}` //set inner html = bookAuthor
+
+         //create div to store the book pages
+         let bookPagesContainer = document.createElement("div");
+        //append book pages div to book card
+        bookCard.append(bookPagesContainer);
+        bookPagesContainer.innerHTML = `Pages: ${bookPages}`//set inner html = bookPages
+
+         //create div to store the book read status
+         let bookReadStatusContainer = document.createElement("div");
+        //append book read status div to book card
+        bookCard.append(bookReadStatusContainer);
+        //set inner html = bookReadStatus
+        bookReadStatusContainer.innerHTML = `Read Status: ${bookReadStatus}`;
+
+        let removeBtn = document.createElement("button");//create a remove button
+        //append it to book card
+        bookCard.append(removeBtn);
+        //give it a class of remove book
+        removeBtn.className = "remove-book";
+        //add a data attribute of removeBtnNumber
+        removeBtn.dataset.removeBtnNumber = index;
+        removeBtn.innerHTML = "Remove Book";//add inner html = remove book
 
 
 
-//remove book card if card number = button number
+        
+    }
+        
+    
+
+}
+
+function removeBook(indexNumber){
+    myLibrary.splice(indexNumber,1);
+
+}
